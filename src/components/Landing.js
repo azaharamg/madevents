241a2a923ff82177b3cd4dc2888082faff9100c8
Azahara <azaharamaga@gmail.com>
2020-01-21 12:30:01 +0100

    Create landing component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Calendar from './landingComponents/Calendar';
+import LinkButton from './landingComponents/LinkButton';
 
 function Landing() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <main>
+      <h1>¿ Qué planes hay ?</h1>
+      <Calendar />
+      <LinkButton />
+      <section>
+        <h2>MadEvents</h2>
+        <p>
+          Es una aplicación web para consultar los eventos en Madrid y poder filtrarlos por distrito o categotías, asi
+          encontrarás facilmente nuevos eventos a los que asistir !!!
+        </p>
+      </section>
+    </main>
   );
 }
 
