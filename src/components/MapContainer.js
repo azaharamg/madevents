4c1d80c61cc98398c3968e7af358c22dc4457c4d
Azahara <azaharamaga@gmail.com>
2020-01-28 13:01:22 +0100

    Show all events with marker in a map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-class MapContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    console.log(this.props.filteredEvents); //[{key}]
-    const firstEvent = this.props.filteredEvents[0];
-    const lat = firstEvent.location.latitude;
-    const lng = firstEvent.location.longitude;
-    return (
-      <Map google={this.props.google} zoom={8} initialCenter={{ lat: 40.416775, lng: -3.70379 }}>
-        <Marker position={{ lat: lat, lng: lng }} />
-      </Map>
-    );
-  }
+function MapContainer(props) {
+  return (
+    <Map google={props.google} zoom={8} initialCenter={{ lat: 40.416775, lng: -3.70379 }}>
+      {props.filteredEvents.map(event => {
+        const lat = event.location.latitude;
+        const lng = event.location.longitude;
+        return <Marker key={event.id} position={{ lat: lat, lng: lng }} />;
+      })}
+    </Map>
+  );
 }
 
 export default GoogleApiWrapper({
