b0b7ad5dd3d719153984fd74049fa16279c55a52
Azahara <azaharamaga@gmail.com>
2020-02-12 11:05:46 +0100

    Add default value in select input

--- src/components/SelectOptions.js
+++ src/components/SelectOptions.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
 function SelectOptions(props) {
-  const handleSelected = event => {
-    props.handleSelected(props.type, event.target.value);
-  };
+	const handleSelected = event => {
+		props.handleSelected(props.type, event.target.value);
+	};
 
-  return (
-    <select onChange={handleSelected}>
-      <option value=''>Todos</option>
-      {props.options.map((option, index) => {
-        return (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        );
-      })}
-    </select>
-  );
+	return (
+		<form className='input-group mb-3'>
+			<select className='custom-select' onChange={handleSelected}>
+				<option defaultValue>
+					Buscar por {props.type === 'selectedDistrict' ? 'distrito' : 'categoría'}...
+				</option>
+				<option value=''>Todos</option>
+				{props.options.map((option, index) => {
+					return (
+						<option key={index} value={option}>
+							{option}
+						</option>
+					);
+				})}
+			</select>
+		</form>
+	);
 }
 
 export default SelectOptions;
