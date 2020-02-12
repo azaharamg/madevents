d4e0a72c83c14cd34cb6ceb55340911b384654a5
Azahara <azaharamaga@gmail.com>
2020-02-12 11:26:28 +0100

    Display markers in google map and adding on click event in the marker to show details

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import EventsDetail from './EventsDetail';
 
-function MapContainer(props) {
-  return (
-    <Map google={props.google} zoom={8} initialCenter={{ lat: 40.416775, lng: -3.70379 }}>
-      {props.filteredEvents.map(event => {
-        const lat = event.location.latitude;
-        const lng = event.location.longitude;
-        return <Marker key={event.id} position={{ lat: lat, lng: lng }} />;
-      })}
-    </Map>
-  );
+class MapContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			markerEvent: ''
+		};
+	}
+
+	render() {
+		const { props } = this;
+		const handleShowdetails = event => {
+			const findElement = props.filteredEvents.find(element => element.id === event.id);
+			this.setState({
+				markerEvent: findElement
+			});
+		};
+
+		return (
+			<section className='row justify-content-center'>
+				<div className='col-3'>
+					{this.state.markerEvent === '' ? (
+						<h5>No hay eventos seleccionados</h5>
+					) : (
+						<EventsDetail markerEvent={this.state.markerEvent} />
+					)}
+				</div>
+				<section className='col-8'>
+					<Map
+						style={{ height: '50vh', width: '50vw' }}
+						google={props.google}
+						zoom={11}
+						initialCenter={{ lat: 40.416775, lng: -3.70379 }}
+					>
+						{props.filteredEvents.map(event => {
+							const lat = event.location.latitude;
+							const lng = event.location.longitude;
+							return (
+								<Marker
+									key={event.id}
+									id={event.id}
+									position={{ lat: lat, lng: lng }}
+									onClick={handleShowdetails}
+								/>
+							);
+						})}
+					</Map>
+				</section>
+			</section>
+		);
+	}
 }
 
 export default GoogleApiWrapper({
-  key: 'YOUR_API_KEY'
+	key: process.env.REACT_APP_MAP_API_KEY
 })(MapContainer);
