a851d27a31b749f3de1d857e843260dc7cef5eb5
Azahara <azaharamaga@gmail.com>
2020-02-27 20:21:54 +0100

    Add link to home

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ButtonGoBack from './ButtonGoBack';
 import SelectOptions from './SelectOptions';
 import MapContainer from './MapContainer';
@@ -7,115 +8,114 @@ import '../stylesheet/mainPage.scss';
 import Logo from '../images/logo.png';
 
 class MainPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			allEvents: props.location.state.events,
-			districts: this.extractDistricts(props.location.state.events),
-			categories: this.extractCategories(props.location.state.events),
-			selectedDistrict: '',
-			selectedCategory: '',
-			markerEvent: null
-		};
-		this.handleSelectedOption = this.handleSelectedOption.bind(this);
-		this.handleShowdetails = this.handleShowdetails.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      allEvents: props.location.state.events,
+      districts: this.extractDistricts(props.location.state.events),
+      categories: this.extractCategories(props.location.state.events),
+      selectedDistrict: '',
+      selectedCategory: '',
+      markerEvent: null
+    };
+    this.handleSelectedOption = this.handleSelectedOption.bind(this);
+    this.handleShowdetails = this.handleShowdetails.bind(this);
+  }
 
-	extractCategories(events) {
-		const filteredByCategories = events.filter(event => event.audience !== undefined).map(event => event.audience);
-		return Array.from(new Set(filteredByCategories));
-	}
+  extractCategories(events) {
+    const filteredByCategories = events.filter(event => event.audience !== undefined).map(event => event.audience);
+    return Array.from(new Set(filteredByCategories));
+  }
 
-	extractDistricts(events) {
-		const filteredByDistricts = events
-			.filter(event => event.address !== undefined)
-			.map(event => this.getDistrictFromEvent(event));
+  extractDistricts(events) {
+    const filteredByDistricts = events
+      .filter(event => event.address !== undefined)
+      .map(event => this.getDistrictFromEvent(event));
 
-		// Remove duplicates
-		return Array.from(new Set(filteredByDistricts));
-	}
+    // Remove duplicates
+    return Array.from(new Set(filteredByDistricts));
+  }
 
-	filterByUserInput(events) {
-		return events
-			.filter(event => {
-				return (
-					this.getDistrictFromEvent(event) === this.state.selectedDistrict ||
-					this.state.selectedDistrict === ''
-				);
-			})
-			.filter(event => {
-				return event.audience === this.state.selectedCategory || this.state.selectedCategory === '';
-			});
-	}
+  filterByUserInput(events) {
+    return events
+      .filter(event => {
+        return this.getDistrictFromEvent(event) === this.state.selectedDistrict || this.state.selectedDistrict === '';
+      })
+      .filter(event => {
+        return event.audience === this.state.selectedCategory || this.state.selectedCategory === '';
+      });
+  }
 
-	getDistrictFromEvent(event) {
-		if (event.address === undefined) {
-			debugger;
-			return undefined;
-		}
-		const url = new URL(event.address.district['@id']);
-		const pathUrl = url.pathname;
-		return pathUrl.substr(pathUrl.lastIndexOf('/') + 1);
-	}
+  getDistrictFromEvent(event) {
+    if (event.address === undefined) {
+      debugger;
+      return undefined;
+    }
+    const url = new URL(event.address.district['@id']);
+    const pathUrl = url.pathname;
+    return pathUrl.substr(pathUrl.lastIndexOf('/') + 1);
+  }
 
-	handleSelectedOption(option, selectedValue) {
-		this.setState({
-			[option]: selectedValue
-		});
-	}
+  handleSelectedOption(option, selectedValue) {
+    this.setState({
+      [option]: selectedValue
+    });
+  }
 
-	handleShowdetails(marker) {
-		const findElement = this.state.allEvents.find(element => element.id === marker.id);
-		this.setState({
-			markerEvent: findElement
-		});
-	}
+  handleShowdetails(marker) {
+    const findElement = this.state.allEvents.find(element => element.id === marker.id);
+    this.setState({
+      markerEvent: findElement
+    });
+  }
 
-	render() {
-		const filteredEvents = this.filterByUserInput(this.state.allEvents);
+  render() {
+    const filteredEvents = this.filterByUserInput(this.state.allEvents);
 
-		return (
-			<main className='container mb-5 col-centered'>
-				<header className='row justify-content-around'>
-					<div className='col-6'>
-						<img className='img-fluid fixed-top' src={Logo} alt='logo' />
-					</div>
-				</header>
-				<div className='row justify-content-center'>
-					<div className='col-6'>
-						<h3>{`Se han encontrado ${filteredEvents.length} eventos`}</h3>
-					</div>
-				</div>
-				<div className='row justify-content-around'>
-					<div className='col-3'>
-						<SelectOptions
-							type='selectedDistrict'
-							options={this.state.districts}
-							handleSelected={this.handleSelectedOption}
-						/>
-					</div>
-					<div className='col-3'>
-						<SelectOptions
-							type='selectedCategory'
-							options={this.state.categories}
-							handleSelected={this.handleSelectedOption}
-						/>
-					</div>
-					<div className='col-3'>
-						<ButtonGoBack />
-					</div>
-				</div>
-				<div className='row'>
-					<div className='col-4'>
-						<EventsDetail markerEvent={this.state.markerEvent} />
-					</div>
-					<div className='col-6'>
-						<MapContainer filteredEvents={filteredEvents} handleShowdetails={this.handleShowdetails} />
-					</div>
-				</div>
-			</main>
-		);
-	}
+    return (
+      <main className='container mb-5 col-centered'>
+        <header className='row justify-content-around'>
+          <div className='col-6'>
+            <Link to='/'>
+              <img className='img-fluid fixed-top' src={Logo} alt='logo' />
+            </Link>
+          </div>
+        </header>
+        <div className='row justify-content-center'>
+          <div className='col-6'>
+            <h3>{`Se han encontrado ${filteredEvents.length} eventos`}</h3>
+          </div>
+        </div>
+        <div className='row justify-content-around'>
+          <div className='col-3'>
+            <SelectOptions
+              type='selectedDistrict'
+              options={this.state.districts}
+              handleSelected={this.handleSelectedOption}
+            />
+          </div>
+          <div className='col-3'>
+            <SelectOptions
+              type='selectedCategory'
+              options={this.state.categories}
+              handleSelected={this.handleSelectedOption}
+            />
+          </div>
+          <div className='col-3'>
+            <ButtonGoBack />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4'>
+            <EventsDetail markerEvent={this.state.markerEvent} />
+          </div>
+          <div className='col-6'>
+            <MapContainer filteredEvents={filteredEvents} handleShowdetails={this.handleShowdetails} />
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default MainPage;
