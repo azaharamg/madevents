bbad988b352c51b6545f1e8e0733da1bd8185a25
Azahara <azaharamaga@gmail.com>
2020-04-11 18:00:06 +0200

    Change Bootstrap to React Bootstrap

--- client/src/components/EventsDetail.js
+++ client/src/components/EventsDetail.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import '../stylesheet/eventDetail.scss';
 
+import Card from 'react-bootstrap/Card';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-const paintCard = markerEvent => {
+const paintCard = (markerEvent) => {
   const { title, time, description, link } = markerEvent;
   const message = `¿Te vienes a este evento? Es sobre ${description} `;
   return (
-    <div className='card'>
-      <div className='card-header'>
-        <h4 className='card-share__title'>{title}</h4>
-        <a
-          className='card-share__button'
-          href={`https://twitter.com/intent/tweet?text=${message}%20%23FunnyEvents%20https://media.giphy.com/media/3EfgWHj0YIDrW/giphy.gif`}
-          title='Comparte en twitter'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <i className='card-share__icon'>
-            <FontAwesomeIcon icon={faTwitter} />
-          </i>
-        </a>
-      </div>
-      <div className='card-body'>
-        {time === '' ? '' : <h5 className='card-title'>Horario: {time}</h5>}
+    <Card className='card'>
+      <Card.Header className='card__header'>
+        <Card.Title>{title}</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        {time === '' ? '' : <h5>Horario: {time}</h5>}
         {description === '' ? (
-          <p className='card-text'>
-            Visite el siguiente{' '}
+          <Card.Text>
+            Visite el siguiente
             <a href={link} target='_blank' rel='noopener noreferrer'>
-              enlace
-            </a>{' '}
-            para más información
-          </p>
-        ) : (
-          <div>
-            <p className='card-text'>
-              {description.length > 200 ? description.substring(0, 199) + '...' : description}
-            </p>
-            <a className='btn btn-primary' href={link} target='_blank' rel='noopener noreferrer'>
-              Saber más
+              {' '}
+              enlace{' '}
             </a>
-          </div>
+            para más información
+          </Card.Text>
+        ) : (
+          <Card.Body>
+            <Card.Text>{description.length > 200 ? description.substring(0, 199) + '...' : description}</Card.Text>
+            <Card.Link className='btn btn-primary' href={link} target='_blank' rel='noopener noreferrer'>
+              Saber más
+            </Card.Link>
+            <Card.Link
+              href={`https://twitter.com/intent/tweet?text=${message}%20%23FunnyEvents%20https://media.giphy.com/media/3EfgWHj0YIDrW/giphy.gif`}
+              title='Comparte en twitter'
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              <i>
+                <FontAwesomeIcon icon={faTwitter} />
+              </i>
+            </Card.Link>
+          </Card.Body>
         )}
-      </div>
-    </div>
+      </Card.Body>
+    </Card>
   );
 };
 
