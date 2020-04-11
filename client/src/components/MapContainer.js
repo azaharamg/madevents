7ba8f5180d946d8343c6103cbd71ac1515ee08e2
Azahara <azaharamaga@gmail.com>
2020-04-11 18:01:34 +0200

    Add styles to map

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import '../stylesheet/mapContainer.scss';
+const style = {
+  width: '100%',
+  height: '50vh',
+};
+
 function MapContainer(props) {
-  const handleShowdetails = marker => {
+  const handleShowdetails = (marker) => {
     props.handleShowdetails(marker);
   };
 
   return (
-    <div className='map'>
-      <Map google={props.google} zoom={11} initialCenter={{ lat: 40.416775, lng: -3.70379 }}>
-        {props.filteredEvents.map(event => {
-          const lat = event.location.latitude;
-          const lng = event.location.longitude;
-          return <Marker key={event.id} id={event.id} position={{ lat: lat, lng: lng }} onClick={handleShowdetails} />;
-        })}
-      </Map>
-    </div>
+    <Map
+      google={props.google}
+      className='map__containter'
+      style={style}
+      zoom={11}
+      initialCenter={{ lat: 40.416775, lng: -3.70379 }}
+    >
+      {props.filteredEvents.map((event) => {
+        const lat = event.location.latitude;
+        const lng = event.location.longitude;
+        return <Marker key={event.id} id={event.id} position={{ lat: lat, lng: lng }} onClick={handleShowdetails} />;
+      })}
+    </Map>
   );
 }
 
 export default GoogleApiWrapper({
-  key: process.env.REACT_APP_MAP_API_KEY
+  key: process.env.REACT_APP_MAP_API_KEY,
 })(MapContainer);
