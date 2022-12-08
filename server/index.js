315bd10e0805a87e13f92974395290376498e2d5
azahara <azaharamaga@gmail.com>
2022-12-08 13:13:46 +0100

    update node-fetch

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const path = require('path');
-const fetch = require('node-fetch');
+const express = require('express')
+const path = require('path')
+// mod.cjs
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
-const app = express();
+const app = express()
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 async function fetchEvents() {
   let response = await fetch('https://datos.madrid.es/egob/catalogo/206974-0-agenda-eventos-culturales-100.json', {
@@ -13,26 +14,26 @@ async function fetchEvents() {
     headers: {
       Accept: 'application/json'
     }
-  });
-  let data = await response.json();
+  })
+  let data = await response.json()
 
   if (!data['@graph']) {
-    return [];
+    return []
   }
 
-  return data['@graph'].filter(event => event.address);
+  return data['@graph'].filter((event) => event.address)
 }
 
 app.get('/api/events', async (req, res) => {
-  const response = await fetchEvents();
-  res.json(response);
-});
+  const response = await fetchEvents()
+  res.json(response)
+})
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const port = process.env.PORT || 5000
+app.listen(port)
